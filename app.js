fe8c49d2b93e5a578f213edcaf3bc1da15a536c8
doceo <diomede.mazzone@gmail.com>
2016-02-05 19:02:43 +0100

    update app e index

--- app.js
+++ app.js
@@ -22,17 +22,7 @@ io.on('connection', function(socket) {
 
   console.log('user connected');
 
-  // a connessione avvenuta,
-  // usa il modulo fs per monitorare il file image.jpg
-  fs.watch(__dirname+'/pictures/image.jpg', function(evt, filename) {
-    console.log('rilevata variazione immagine');
 
-    // in caso di variazione del file image.jpg lo reinvia
-    // al client tramite il socket precedentemente creato
-    fs.readFile(__dirname+'/pictures/image.jpg', function(err, buf) {
-      socket.emit('update_image', {image: true, buffer: buf.toString('base64')});
-    });
-  });
 });
 
 console.log('server listening on port 3000');
