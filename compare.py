4bc52062193a3754e2e0d9a1b6dd2a6f903c155d
Diomede Mazzone <diomede.mazzone@gmail.com>
2016-02-16 09:40:47 +0100

    major fix

--- compare.py
+++ compare.py
@@ -1,31 +1,21 @@
-import cv2
 import numpy as np
-from matplotlib import pyplot as plt
+import cv2
+ 
+image = cv2.imread('/pictures/acquisita.jpg')
+template = cv2.imread('/pictures/confronto.jpg')
+ 
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+ 
+result = cv2.matchTemplate(image,template, cv2.TM_CCOEFF)
+min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+top_left = max_loc
 
-img = cv2.imread('tutte.jpg',0)
-img2 = img.copy()
-template = cv2.imread('quadrato.jpg',0)
-w, h = template.shape[::-1]
+print max_loc //stampo questa variabile per cercare una relazione tra i falsi positivi
 
-# All the 6 methods for comparison in a list
-methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
-            'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
-
-for meth in methods:
-    img = img2.copy()
-    method = eval(meth)
-
-    # Apply template Matching
-    res = cv2.matchTemplate(img,template,method)
-    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-
-    # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-    if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
-        top_left = min_loc
-    else:
-        top_left = max_loc
-    bottom_right = (top_left[0] + w, top_left[1] + h)
-
-    img = cv2.rectangle(img,top_left, bottom_right, 255, 2)
-    cv2.imwrite('risultato.jpg',img)
-
+h,w = template.shape
+bottom_right = (top_left[0] + w, top_left[1] + h)
+cv2.rectangle(image,top_left, bottom_right,(0,0,255),3)
+ 
+cv2.imshow("Result", image)
+cv2.waitKey(0)
