e5facd4c2e91df0b73d50f06c82edc00be2e2631
Diomede Mazzone <diomede.mazzone@gmail.com>
2016-02-22 10:39:03 +0100

    add eyeDetect

---
+++ eyeDetect.py
@@ -0,0 +1,32 @@
+import cv2
+import sys
+
+# Get user supplied values
+imagePath = sys.argv[1]
+cascPath = sys.argv[2]
+
+# Create the haar cascade
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+# Read the image
+image = cv2.imread(imagePath)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Detect eye in the image
+eye = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(30, 30),
+    flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+)
+
+print "Found {0} eye!".format(len(eye))
+
+# Draw a rectangle around the faces
+for (x, y, w, h) in eye:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+#cv2.imshow("eye found", image)
+cv2.imwrite('pictures/image_result.jpg', image)
+cv2.waitKey(0)
